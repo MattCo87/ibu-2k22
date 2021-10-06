9f55035706e82d5e66735e60813a637c36cfb663
matt co <87700a@gmail>
2021-10-06 23:23:05 +0200

    Init table co

---
+++ src/Datafixtures/DistanceFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Distance;
+use App\Repository\DistanceRepository;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+
+class DistanceFixtures extends Fixture implements OrderedFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        /**
+         * Default distance
+         */
+        $tabdistance = [];
+        $tabdistance = [
+            "15 Km",
+            "20 Km", 
+            "7.5 Km", 
+            "10 Km",
+            "4 X 7.5 Km",
+            "4 x 6 Km",
+            "12.5 Km",
+        ];
+        
+        foreach ($tabdistance as &$value)
+        {
+            $distance = new Distance();
+            $distance->setName($value);
+            $manager->persist($distance);
+            $this->addReference($distance->getName(), $distance);
+        }
+        unset($value, $tabdistance, $distance);
+
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 4;
+    }
+}
---
+++ src/Entity/Country.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CountryRepository::class)
+ */
+class Country
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $flag;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Stage::class, mappedBy="country")
+     */
+    private $stages;
+
+    public function __construct()
+    {
+        $this->stages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFlag(): ?string
+    {
+        return $this->flag;
+    }
+
+    public function setFlag(?string $flag): self
+    {
+        $this->flag = $flag;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Stage[]
+     */
+    public function getStages(): Collection
+    {
+        return $this->stages;
+    }
+
+    public function addStage(Stage $stage): self
+    {
+        if (!$this->stages->contains($stage)) {
+            $this->stages[] = $stage;
+            $stage->setCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function removeStage(Stage $stage): self
+    {
+        if ($this->stages->removeElement($stage)) {
+            // set the owning side to null (unless already changed)
+            if ($stage->getCountry() === $this) {
+                $stage->setCountry(null);
+            }
+        }
+
+        return $this;
+    }
+}
