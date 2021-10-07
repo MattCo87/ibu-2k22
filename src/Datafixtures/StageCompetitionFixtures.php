9e254f752de96140dfd51457ac53aca7c1457c53
matt co <87700a@gmail>
2021-10-07 02:01:07 +0200

    Fix Fixture

--- src/Datafixtures/StageCompetitionFixtures.php
+++ src/Datafixtures/StageCompetitionFixtures.php
@@ -15,7 +15,21 @@ class StageCompetitionFixtures extends Fixture implements OrderedFixtureInterfac
     {
         /**
          * Default run
+         * 
+         * Competition1 :  ["Femme", "15 Km", "Individuel"]
+         * Competition2 :  ["Femme", "7.5 Km", "Sprint"]
+         * Competition3 :  ["Femme", "10 Km", "Poursuite"]
+         * Competition4 :  ["Femme", "4 x 6 Km", "Relais"]
+         * Competition5 :  ["Femme", "12.5 Km", "Mass Start"]
+         * Competition6 :  ["Homme", "20 Km", "Individuel"]
+         * Competition7 :  ["Homme", "10 Km", "Sprint"]
+         * Competition8 :  ["Homme", "12.5 Km", "Poursuite"]
+         * Competition9 :  ["Homme", "4 X 7.5 Km", "Relais"]
+         * Competition10 : ["Homme", "15 Km", "Mass Start"]
+         * Competition11 : ["Mixte", "4 X 7.5 Km", "Relais"]
+         * Competition12 : ["Mixte", "Simple", "Relais"]
          */
+
         $tabrun = [];
         $tabrun = [
             ["Oestersund", "Competition1", "21-11-27"],
