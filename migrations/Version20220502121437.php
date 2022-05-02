ee41222ff6af4e1cf476101d052e5b26bc8d8f57
MattCo87 <77285498+MattCo87@users.noreply.github.com>
2022-05-02 14:53:20 +0200

    Install login

--- migrations/Version20220502121437.php
+++ migrations/Version20220502121437.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211009181149 extends AbstractMigration
+final class Version20220502121437 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,12 +22,14 @@ final class Version20211009181149 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE athlete (id INT AUTO_INCREMENT NOT NULL, gender_id INT DEFAULT NULL, country_id INT DEFAULT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, INDEX IDX_C03B8321708A0E0 (gender_id), INDEX IDX_C03B8321F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE competition (id INT AUTO_INCREMENT NOT NULL, style_id INT NOT NULL, gender_id INT NOT NULL, name VARCHAR(255) NOT NULL, step_distance DOUBLE PRECISION NOT NULL, step_number INT NOT NULL, shot_number INT NOT NULL, INDEX IDX_B50A2CB1BACD6074 (style_id), INDEX IDX_B50A2CB1708A0E0 (gender_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE config (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, flag VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE gender (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE run (id INT AUTO_INCREMENT NOT NULL, stage_id INT DEFAULT NULL, athlete_id INT DEFAULT NULL, competition_id INT DEFAULT NULL, shot_id INT DEFAULT NULL, zone_id INT DEFAULT NULL, date_run DATE DEFAULT NULL, step_run INT DEFAULT NULL, result_run INT DEFAULT NULL, hour_run VARCHAR(255) DEFAULT NULL, INDEX IDX_5076A4C02298D193 (stage_id), INDEX IDX_5076A4C0FE6BCB8B (athlete_id), INDEX IDX_5076A4C07B39D312 (competition_id), INDEX IDX_5076A4C0C274538A (shot_id), INDEX IDX_5076A4C09F2C3FAB (zone_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE shot (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE stage (id INT AUTO_INCREMENT NOT NULL, country_id INT NOT NULL, name VARCHAR(255) NOT NULL, INDEX IDX_C27C9369F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE style (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE zone (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE zone_stage (zone_id INT NOT NULL, stage_id INT NOT NULL, INDEX IDX_64523CCF9F2C3FAB (zone_id), INDEX IDX_64523CCF2298D193 (stage_id), PRIMARY KEY(zone_id, stage_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE athlete ADD CONSTRAINT FK_C03B8321708A0E0 FOREIGN KEY (gender_id) REFERENCES gender (id)');
@@ -61,12 +63,14 @@ final class Version20211009181149 extends AbstractMigration
         $this->addSql('ALTER TABLE zone_stage DROP FOREIGN KEY FK_64523CCF9F2C3FAB');
         $this->addSql('DROP TABLE athlete');
         $this->addSql('DROP TABLE competition');
+        $this->addSql('DROP TABLE config');
         $this->addSql('DROP TABLE country');
         $this->addSql('DROP TABLE gender');
         $this->addSql('DROP TABLE run');
         $this->addSql('DROP TABLE shot');
         $this->addSql('DROP TABLE stage');
         $this->addSql('DROP TABLE style');
+        $this->addSql('DROP TABLE user');
         $this->addSql('DROP TABLE zone');
         $this->addSql('DROP TABLE zone_stage');
     }
