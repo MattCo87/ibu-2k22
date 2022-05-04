c3a7c6de85cf9cb400f6cd5c8b444018816b08fc
MattCo87 <77285498+MattCo87@users.noreply.github.com>
2022-05-04 04:06:13 +0200

    Liste athlète v2

--- src/Controller/AthleteController.php
+++ src/Controller/AthleteController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use App\Repository\AthleteRepository;
 use App\Repository\UserRepository;
 use App\Entity\Athlete;
+use App\Form\AthleteType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Request;
 
 class AthleteController extends AbstractController
 {
@@ -26,21 +28,37 @@ class AthleteController extends AbstractController
     /**
      * @Route("/athlete/profil/{id}", name="app_athlete_profil")
      */
-    public function AthleteProfil(): Response
+    public function AthleteProfil(Athlete $athlete): Response
     {
+        $var_athlete = $this->ema->findOneBy(['id' => $athlete]);
+
         return $this->render('athlete/profil.html.twig', [
-            'controller_name' => 'AthleteProfil',
+            'athlete' => $var_athlete,
         ]);
     }
 
     /**
      * @Route("/athlete/list", name="app_athlete_choose")
      */
-    public function ListProfil(): Response
+    public function ListProfil(Request $request): Response
     {
         $athletes = $this->ema->findAll();
+        $var_athlete = new Athlete();
+        $form = $this->createForm(AthleteType::class, $var_athlete);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($form->getData()->getGender()->getId() != 3 && $form->getData()->getCountry()->getId() != 1) {
+                $athletes = $this->ema->findBy(['gender' => $form->getData()->getGender(), 'country' => $form->getData()->getCountry()]);
+
+                //dd($form->getData());
+            } 
+            
+        }
+
         return $this->render('athlete/list.html.twig', [
             'athletes' => $athletes,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -51,9 +69,7 @@ class AthleteController extends AbstractController
     {
         $user = $this->security->getUser()->getId();
         $this->emu->addAthlete($user, $athlete);
-     
-        return $this->render('home/index.html.twig', [
-    
-        ]);
+
+        return $this->render('home/index.html.twig', []);
     }
 }
